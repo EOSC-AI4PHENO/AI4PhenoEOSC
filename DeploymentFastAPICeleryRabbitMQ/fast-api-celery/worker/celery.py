5736d9acdb087cbb3f0073b89288c864d427a868
Jaroslaw Kurek <jaroslaw.kurek@dybusinfo.com>
2023-07-23 00:51:10 +0200

    2023-07-23 00:51

--- DeploymentFastAPICeleryRabbitMQ/fast-api-celery/worker/celery.py
+++ DeploymentFastAPICeleryRabbitMQ/fast-api-celery/worker/celery.py
@@ -1,7 +1,7 @@
 from celery import Celery
 import os
 
-worker = Celery("proj", backend=os.getenv("CELERY_BACKEND_URL"), broker=os.getenv("CELERY_BROKER_URL"), include=["worker.tasks", "logic"], task_serializer='pickle', accept_content=['pickle'])
+worker = Celery("myapp", backend=os.getenv("CELERY_BACKEND_URL"), broker=os.getenv("CELERY_BROKER_URL"), include=["worker.tasks", "logic"], task_serializer='pickle',  result_serializer='pickle', accept_content=['pickle'])
 
 # Optional configuration, see the application user guide.
 worker.conf.update(
