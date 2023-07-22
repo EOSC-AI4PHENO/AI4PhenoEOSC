1445b8e33ae0ef428285876a1a565543ac1b7422
Jaroslaw Kurek <jaroslaw.kurek@dybusinfo.com>
2023-07-22 18:21:02 +0200

    2023-07-22 18:21

--- DeploymentFastAPICeleryRabbitMQ/fast-api-celery/logic/model.py
+++ DeploymentFastAPICeleryRabbitMQ/fast-api-celery/logic/model.py
@@ -1,6 +1,23 @@
-class FakeModel:
+from astral import Observer
+import pytz
+from astral.sun import sun
+
+# class FakeModel:
+#     def __init__(self):
+#         self.m = 7.0
+#         self.q = 0.5
+#     def predict(self, x):
+#         return self.m * x + self.q
+#
+class WellExposedModel:
     def __init__(self):
         self.m = 7.0
         self.q = 0.5
-    def predict(self, x):
-        return self.m * x + self.q
+    def get_sunrise_sunset(self,lat, lon, UTCdate):
+        observer = Observer(latitude=lat, longitude=lon)
+        s = sun(observer, date=UTCdate)
+
+        UTCsunrise = s['sunrise'].astimezone(pytz.timezone('UTC'))
+        UTCsunset = s['sunset'].astimezone(pytz.timezone('UTC'))
+
+        return UTCsunrise, UTCsunset
