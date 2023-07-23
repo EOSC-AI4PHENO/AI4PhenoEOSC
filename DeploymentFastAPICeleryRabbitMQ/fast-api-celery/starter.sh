70e3152e6d75ffc0929a40dafc01d7c60cf5d40c
Jaroslaw Kurek <jaroslaw.kurek@dybusinfo.com>
2023-07-23 07:03:59 +0200

    2023-07-23 0:03

--- DeploymentFastAPICeleryRabbitMQ/fast-api-celery/starter.sh
+++ DeploymentFastAPICeleryRabbitMQ/fast-api-celery/starter.sh
@@ -8,7 +8,7 @@ while [ $# -gt 0 ] ; do
 done
 
 case $W in
-    worker) celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery worker --loglevel=INFO;;
+    worker) celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery worker --loglevel=DEBUG;;
     flower) celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery flower;;
     fastapi) uvicorn api.main:app --host 0.0.0.0 --port 80;;
 esac
--- DeploymentFastAPICeleryRabbitMQ/fast-api-celery/worker/celery.py
+++ DeploymentFastAPICeleryRabbitMQ/fast-api-celery/worker/celery.py
@@ -1,17 +1,18 @@
 from celery import Celery
 import os
 
-worker = Celery("myapp", backend=os.getenv("CELERY_BACKEND_URL"), broker=os.getenv("CELERY_BROKER_URL"), include=["worker.tasks", "logic"], task_serializer='pickle',  result_serializer='pickle', accept_content=['pickle'])
+worker = Celery("myapp", backend=os.getenv("CELERY_BACKEND_URL"), broker=os.getenv("CELERY_BROKER_URL"),
+                include=["worker.tasks", "logic"], task_serializer='pickle', result_serializer='pickle',
+                accept_content=['pickle', 'json'])
 
-#worker = Celery("myapp", backend=os.getenv("CELERY_BACKEND_URL"), broker=os.getenv("CELERY_BROKER_URL"), include=["worker.tasks", "logic"])
+# worker = Celery("myapp", backend=os.getenv("CELERY_BACKEND_URL"), broker=os.getenv("CELERY_BROKER_URL"), include=["worker.tasks", "logic"])
 
-worker.conf.task_serializer = 'pickle'
-worker.conf.result_serializer = 'pickle'
-worker.conf.event_serializer = 'pickle'
-worker.conf.accept_content = ['pickle']
-worker.conf.task_accept_content = ['pickle']
-worker.conf.result_accept_content = ['pickle']
-worker.conf.event_accept_content = ['pickle']
+worker.conf.update(task_track_started=True)
+worker.conf.update(task_serializer='pickle')
+worker.conf.update(result_serializer='pickle')
+worker.conf.update(accept_content=['pickle', 'json'])
+worker.conf.update(result_persistent=True)
+worker.conf.update(worker_send_task_events=False)
 
 # Optional configuration, see the application user guide.
 worker.conf.update(
