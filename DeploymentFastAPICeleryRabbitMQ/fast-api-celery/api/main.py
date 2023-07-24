e0db7a9ab1e7ef517945ca7bcc8abf69cf358dc5
Jaroslaw Kurek <jaroslaw.kurek@dybusinfo.com>
2023-07-24 16:24:22 +0200

    2023-07-24 16:24

--- DeploymentFastAPICeleryRabbitMQ/fast-api-celery/api/main.py
+++ DeploymentFastAPICeleryRabbitMQ/fast-api-celery/api/main.py
@@ -14,6 +14,17 @@ async def schedule_ImageWellExposedModel_is_Image_WellExposedByHisto(model_input
     task_id = is_Image_WellExposedByHisto.delay(model_input.imageRGB, model_input.lat, model_input.lon, model_input.UTCdate)
     return {'task_id': str(task_id), 'status': 'Processing'}
 
+@app.get('/ImageWellExposedModel/is_Image_WellExposedByHisto_result/{task_id}', response_model=ImageWellExposedOutput, status_code=200,
+         responses={202: {'model': TaskTicket, 'description': 'Accepted: Not Ready'}})
+async def get_ImageWellExposedModel_is_Image_WellExposedByHisto_result(task_id):
+    """Fetch result for given task_id"""
+    task = AsyncResult(task_id)
+    if not task.ready():
+        #print(app.url_path_for('schedule_prediction'))
+        return JSONResponse(status_code=202, content={'task_id': str(task_id), 'status': 'Processing'})
+    result = task.get()
+    return {'task_id': task_id, 'status': 'Success', 'result': result}
+
 
 @app.post('/ImageWellExposedModel/get_sunrise_sunset', response_model=TaskTicket, status_code=202)
 async def schedule_ImageWellExposedModel_get_sunrise_sunset(model_input: SunriseSunsetInput):
@@ -26,7 +37,7 @@ async def schedule_ImageWellExposedModel_get_sunrise_sunset(model_input: Sunrise
     return {'task_id': str(task_id), 'status': 'Processing'}
 
 
-@app.get('/ImageWellExposedModel/result/{task_id}', response_model=SunriseSunsetOutput, status_code=200,
+@app.get('/ImageWellExposedModel/get_sunrise_sunset_result/{task_id}', response_model=SunriseSunsetOutput, status_code=200,
          responses={202: {'model': TaskTicket, 'description': 'Accepted: Not Ready'}})
 async def get_ImageWellExposedModel_get_sunrise_sunset_result(task_id):
     """Fetch result for given task_id"""
