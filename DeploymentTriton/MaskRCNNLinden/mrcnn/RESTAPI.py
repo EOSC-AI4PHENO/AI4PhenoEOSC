a83da6a52523c6521d5f72de2e92d5d6ea311090
Jaroslaw Kurek <jaroslaw.kurek@dybusinfo.com>
2023-08-02 05:20:06 +0200

    202308-02 05:20

---
+++ DeploymentTriton/MaskRCNNLinden/mrcnn/RESTAPI.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import numpy as np
+
+def query_triton_model(molded_images: np.ndarray, image_metas: np.ndarray, anchors: np.ndarray):
+    # Konwertuj dane wejściowe do listy (format obsługiwany przez JSON)
+    input_image_data = molded_images.tolist()
+    input_image_meta_data = image_metas.tolist()
+    input_anchors_data = anchors.tolist()
+
+    # Tworzymy dane wejściowe
+    data = {
+        "inputs": [
+            {"name": "input_image", "data": input_image_data, "datatype": "FP32", "shape": molded_images.shape},
+            {"name": "input_image_meta", "data": input_image_meta_data, "datatype": "FP32", "shape": image_metas.shape},
+            {"name": "input_anchors", "data": input_anchors_data, "datatype": "FP32", "shape": anchors.shape},
+        ]
+    }
+
+    # Adres serwera Triton (zamień na prawdziwy adres serwera)
+    #triton_server_url = "http://10.0.20.50:8050/v2/models/AppleMaskRCNNModel/infer"
+
+    triton_server_url = "http://10.0.20.50:8000/v2/models/AppleMaskRCNNModel/versions/1/infer"
+
+    # Wykonujemy zapytanie POST do serwera Triton
+    response = requests.post(triton_server_url, data=json.dumps(data))
+
+    # Pobieramy odpowiedź od serwera
+    response_data = response.json()
+
+    # Zwracamy dane wyjściowe
+    ROI = response_data['outputs'][0]['data']
+    mrcnn_bbox = response_data['outputs'][1]['data']
+    mrcnn_class = response_data['outputs'][2]['data']
+    mrcnn_detection = response_data['outputs'][3]['data']
+    mrcnn_mask = response_data['outputs'][4]['data']
+    rpn_bbox = response_data['outputs'][5]['data']
+    rpn_class = response_data['outputs'][6]['data']
+
+    return ROI, mrcnn_bbox, mrcnn_class, mrcnn_detection, mrcnn_mask, rpn_bbox, rpn_class
---
+++ DeploymentTriton/MaskRCNNLinden/mrcnn/grpcJarek.py
@@ -0,0 +1,33 @@
+import grpc
+import numpy as np
+import tensorflow as tf
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+def get_GRPC_prediction(molded_images, image_metas, anchors):
+    channel = grpc.insecure_channel('10.0.20.50:8001')  # Podmień na właściwy adres serwera
+    stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'AppleMaskRCNNModel'
+    request.model_spec.signature_name = 'serving_default'  # Podmień na właściwą nazwę sygnatury, jeśli to konieczne
+
+    # Przekształć dane wejściowe w odpowiedni format
+    request.inputs['input_image'].CopyFrom(
+        tf.make_tensor_proto(molded_images, shape=molded_images.shape))
+    request.inputs['input_anchors'].CopyFrom(
+        tf.make_tensor_proto(anchors, shape=anchors.shape))
+    request.inputs['input_image_meta'].CopyFrom(
+        tf.make_tensor_proto(image_metas, shape=image_metas.shape))
+
+    response = stub.Predict(request, 10.0)  # 10 sekundowy limit czasu
+
+    # Zwróć odpowiednie dane wyjściowe
+    ROI = np.array(response.outputs['ROI'].float_val)
+    mrcnn_bbox = np.array(response.outputs['mrcnn_bbox'].float_val)
+    mrcnn_class = np.array(response.outputs['mrcnn_class'].float_val)
+    mrcnn_detection = np.array(response.outputs['mrcnn_detection'].float_val)
+    mrcnn_mask = np.array(response.outputs['mrcnn_mask'].float_val)
+    rpn_bbox = np.array(response.outputs['rpn_bbox'].float_val)
+    rpn_class = np.array(response.outputs['rpn_class'].float_val)
+
+    return ROI, mrcnn_bbox, mrcnn_class, mrcnn_detection, mrcnn_mask, rpn_bbox, rpn_class
