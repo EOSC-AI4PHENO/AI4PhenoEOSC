36a0f38f8513b196d00a36f5eaf3215e6c506a9f
Jaroslaw Kurek <jaroslaw.kurek@dybusinfo.com>
2023-09-12 13:36:57 +0200

    2023-09-12 13:36

--- DeploymentFastAPICeleryRabbitMQ/fast-api-celery/starter.sh
+++ DeploymentFastAPICeleryRabbitMQ/fast-api-celery/starter.sh
@@ -8,7 +8,11 @@ while [ $# -gt 0 ] ; do
 done
 
 case $W in
-    worker) celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery worker --loglevel=INFO;;
-    flower) celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery flower;;
-    fastapi) uvicorn api.main:app --host 0.0.0.0 --port 80;;
+    worker)
+      celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery worker --loglevel=INFO;;
+    flower)
+      export FLOWER_UNAUTHENTICATED_API=true
+      celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery flower;;
+    fastapi)
+      uvicorn api.main:app --host 0.0.0.0 --port 80;;
 esac
