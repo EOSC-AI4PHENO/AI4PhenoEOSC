9664d01fe5e309590534fb3d476ef001ce2cf4c0
Jaroslaw Kurek <jaroslaw.kurek@dybusinfo.com>
2023-09-13 05:32:52 +0200

    2023-09-13 05:32

--- DeploymentFastAPICeleryRabbitMQ/fast-api-celery/starter.sh
+++ DeploymentFastAPICeleryRabbitMQ/fast-api-celery/starter.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-run_worker() {
-  celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery worker --loglevel=INFO --pool-restarts -Q image_exposure_queue,sunrise_sunset_queue,apple_segment_queue,apple_detectron2_queue,linden_class_queue,linden_segment_queue
-}
-
-run_flower() {
-  export FLOWER_UNAUTHENTICATED_API=true
-  celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery flower
-}
-
-run_fastapi() {
-  uvicorn api.main:app --host 0.0.0.0 --port 80
-}
-
 while [ $# -gt 0 ] ; do
   case $1 in
     -t | --target) W="$2" ;;
@@ -21,7 +8,7 @@ while [ $# -gt 0 ] ; do
 done
 
 case $W in
-    worker) run_worker ;;
-    flower) run_flower ;;
-    fastapi) run_fastapi ;;
+    worker) celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery worker --loglevel=INFO;;
+    flower) celery --broker ${CELERY_BROKER_URL} --result-backend ${CELERY_BACKEND_URL} -A worker.celery flower;;
+    fastapi) uvicorn api.main:app --host 0.0.0.0 --port 80;;
 esac
